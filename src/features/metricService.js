2cb92c85330d7599d77ee199943e2ec72080bd68
John Capovani <john@capovani.com>
2023-03-28 23:13:54 -0400

    Updated URLS for API calls

--- src/features/metricService.js
+++ src/features/metricService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://tikerwatch.onrender.com/api';
 
 // Get all stocks
 export const getAllStocks = async (token) => {
